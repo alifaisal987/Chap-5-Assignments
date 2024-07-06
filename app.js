eaf66c237abf0cd5f2ec19e067bac1a093a2bb2c
alifaisal987 <alifaisal987@gmail.com>
2024-07-06 22:57:13 +0500

    done assignment 5

---
+++ app.js
@@ -0,0 +1,126 @@
+// //question 1
+ //1. Write a program that take two numbers & add them in a  new variable. Show the result in your browser.
+
+ var num1= 20
+ console.log(num1)
+
+ var num2=30
+ console.log(num2)
+
+ sum=num1+num2;
+ console.log("addition is" + '' +sum);
+
+
+ //2. Repeat task1 for subtraction, multiplication, division &  modulus.
+
+ var num1=20
+ console.log(num1)
+
+ var num2=30
+ console.log(num2)
+
+ sum=num1-num2;
+ console.log('subtraction is' + sum);
+
+ var num1=20
+ console.log(num1)
+
+ var num2=30
+ console.log(num2)
+
+ sum=num1*num2;
+ console.log('multiplication is' + sum);
+
+ var num1=20
+ console.log(num1)
+
+ var num2=30
+ console.log(num2)
+
+ sum=num1/num2;
+ console.log('division  is' + sum);
+
+
+ var num1=20
+ console.log(num1)
+
+ var num2=30
+ console.log(num2)
+
+ sum=num1%num2;
+ console.log('modulus is' + sum);
+
+
+//  Do the following using JS Mathematic Expressions
+// a. Declare a variable.
+
+var number=5;
+// Show the value of variable in your browser like “Value
+// after variable declaration is: ??”.
+console.log('variable after value declaraction is' +number)
+
+// Increment the variable
+
+var Increment=number+1;
+// Show the value of variable in your browser like “Value
+// after increment is: 6”
+console.log('incrmented value is ' +Increment)
+// . Add 7 to the variable.
+
+var add7 =number+7;
+console.log('after adding 7 number is' +add7)
+
+
+//movie ticket question 4
+
+var movieTicket =600
+
+var ticketQuantitiy = +prompt('Enter number of tickets to buy')
+
+var ticketprice= movieTicket*ticketQuantitiy
+alert('ticket price for' +ticketQuantitiy +'is' +ticketprice)
+
+
+// // Write a script to display multiplication table of any
+// // number in your browser. E.g
+
+// var numMulti=+prompt('Enter number for the table')
+
+// . The Temperature Converter: It’s hot out! Let’s make a
+// converter based on the steps here.
+// a. Store a Celsius temperature into a variable.
+// b. Convert it to Fahrenheit & output “NNoC is NNoF”.
+// c. Now store a Fahrenheit temperature into a variable.
+// d. Convert it to Celsius & output “NNoF is NNoC”.
+
+var celcius= +prompt("Enter temperature in celcuis")
+
+
+var cnof= celcius*9/5 +32;
+alert('temp in farheiniet is' +cnof)
+
+
+var farhaneit= +prompt("Enter temperature in farhaneit")
+
+
+var fnoc= farhaneit-32*5/9
+alert('temp in celcius is' +fnoc)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
